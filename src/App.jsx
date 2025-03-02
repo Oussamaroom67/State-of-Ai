96a6b93154ca2cf5f983a25093cebff999be8fa7
omaima <siafomaima5@gmail.com>
2025-03-02 00:48:41 +0000

    first version Data Insights part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 import LandingPage from './pages/LandingPage';
+import DataInsights from './components/landingPage/DataInsights';
 function App() {
 
   return (
     <>
       <LandingPage/>
-      
+      <DataInsights/>
     </>
   )
 }
