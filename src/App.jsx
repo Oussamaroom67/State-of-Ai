1fc702db1e67e5d341d7581514e963991131f9a2
omaima <siafomaima5@gmail.com>
2025-03-02 12:44:07 +0000

    first version Partner With Us part

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import './App.css';
 import ContentComp from './components/landingPage/ContentComp';
 import ContributionPage from './pages/ContributionPage';
 import LandingPage from './pages/LandingPage';
 import DataInsights from './components/landingPage/DataInsights';
+import CardStats from './components/landingPage/CardStats';
+import PartnerWithUs from './components/landingPage/PartnerWithUs';
 function App() {
 
   return (
     <>
       <LandingPage/>
-      
+      <DataInsights/>
+      <PartnerWithUs/>
     </>
   )
 }
